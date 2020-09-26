12e537d8f09cd43793b457bfe9f7cb6165e97514
Malcolm Anderson <malcolminyo@gmail.com>
2020-09-25 19:18:39 -0700

    Initial Commit

---
+++ Canvas/CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasApp.swift
+//  Canvas
+//
+//  Created by Test Account on 9/25/20.
+//
+
+import SwiftUI
+
+@main
+struct CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Canvas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Canvas
+//
+//  Created by Test Account on 9/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
