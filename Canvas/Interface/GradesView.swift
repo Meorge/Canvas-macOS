244b29c74b23f1985b26dca445e9fc353b407ae1
Malcolm Anderson <malcolminyo@gmail.com>
2021-01-24 14:27:07 -0800

    View grade distribution stats

--- Canvas/Interface/GradesView.swift
+++ Canvas/Interface/GradesView.swift
@@ -23,10 +23,9 @@ struct GradesView: View {
             Divider()
             List {
                 ForEach(self.course.assignmentGroups ?? [], id: \.id) { group in
-                    Section(header: AssignmentGroupHeaderView(assignmentGroup: group)) {
+                    Section(header: AssignmentGroupHeaderView(assignmentGroup: group), footer: Divider()) {
                         ForEach(group.assignments ?? [], id: \.id) { assignment in
                             SingleGradeRowView(assignment: assignment)
-                            
                         }
                     }
                 }
@@ -71,21 +70,46 @@ struct AssignmentGroupHeaderView: View {
 
 struct SingleGradeRowView: View {
     @ObservedObject var assignment: Assignment
+    @State var detailsOpen: Bool = false
     var body: some View {
-        HStack(alignment: .center) {
-            VStack(alignment: .leading) {
-                Text(self.assignment.name ?? "Assignment")
-                    .font(.title3)
-                    .bold()
-                Text(self.assignment.getFormattedDueDate())
-                    .font(.subheadline)
+        Button(action: { self.detailsOpen.toggle() }) {
+            VStack {
+                HStack(alignment: .center) {
+                    VStack(alignment: .leading) {
+                        Text(self.assignment.name ?? "Assignment")
+                            .font(.title3)
+                            .bold()
+                        Text(self.assignment.getFormattedDueDate())
+                            .font(.subheadline)
+                    }
+                    Spacer()
+                    Text(self.assignment.getScoreAsString())
+                        .font(.title3)
+                        .bold()
+                }
+                
+                if self.detailsOpen {
+                    HStack(alignment: .center) {
+                        Spacer()
+                        Text("\(minAsString) - \(meanAsString) - \(maxAsString)")
+                    }
+                }
             }
-            Spacer()
-            Text(self.assignment.getScoreAsString())
-                .font(.title3)
-                .bold()
         }
+        .buttonStyle(PlainButtonStyle())
         .padding(0)
+    }
+    
+    var minAsString: String {
+        self.assignment.scoreStatistics?.min != nil ? self.assignment.scoreStatistics!.min!.removeTrailingZeroes() : "Unknown"
+    }
+    
+    var maxAsString: String {
+        self.assignment.scoreStatistics?.max != nil ? self.assignment.scoreStatistics!.max!.removeTrailingZeroes() : "Unknown"
+    }
+    
+    var meanAsString: String {
+        self.assignment.scoreStatistics?.mean != nil ? self.assignment.scoreStatistics!.mean!.removeTrailingZeroes() : "Unknown"
     }
     
 
